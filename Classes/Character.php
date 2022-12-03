6e445259159e687a132c164b94633d8cb5290edd
Dan Sebag <dan.sebag1007@gmail.com>
2022-12-03 15:57:56 +0100

    add spells and bonus attack in terms of elementaryType and game loop to find out who is the winner

---
+++ Classes/Character.php
@@ -0,0 +1,217 @@
+<?php
+namespace App\Classes;
+
+use Weapon;
+
+abstract class Character
+{
+    public function __construct(
+        protected string $name,
+        protected string $attacktType,
+        protected string $elementarytType,
+        protected int $health,
+        protected int $physicalAttack,
+        protected int $magicalAttack,
+        protected int $defence,
+        protected int $mana,
+        protected int $manaRecoveryRate,
+        protected ?Weapon $weapon,
+        protected ?Spell $spell,
+        protected bool $isProtectedByDefenceSpell,
+        protected bool $bonusAttack,
+    )
+    {
+        $this->name = $name;
+        $this->attacktType = $attacktType;
+        $this->elementarytType = $elementarytType;
+        $this->health = $health;
+        $this->physicalAttack = $physicalAttack;
+        $this->magicalAttack = $magicalAttack;
+        $this->defence = $defence;
+        $this->mana = $mana;
+        $this->manaRecoveryRate = $manaRecoveryRate;
+        $this->weapon = $weapon;
+        $this->spell = $spell;
+        $this->isProtectedByDefenceSpell = false;
+        $this->bonusAttack = false;
+    }
+
+    //Getter & Setter for normals attributs
+    public function getElementaryType()
+    {
+        return $this->elementarytType;
+    }
+
+    public function getHealth(){
+        return $this->health;
+    }
+
+    public function setHealth(int $health){
+        $this->health = $health;
+    }
+
+    public function getDefence(){
+        return $this->defence;
+    }
+
+    public function setDefence(int $defence){
+        $this->defence = $defence;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    //Getter & Setter for Spell
+    public function getSpell(){
+        return $this->spell;
+    }
+
+    public function setSpell(Spell $spell){
+        $this->spell = $spell;
+    }
+
+    public function setAttackSpells(array $array)
+    {
+        $this->attackSpells = $array;
+    }
+
+    public function getAttackSpells()
+    {
+        return $this->attackSpells;
+    }
+
+    public function getIsProtectedByDefenceSpell(){
+        return $this->isProtectedByDefenceSpell;
+    }
+
+    public function setIsProtectedByDefenceSpell(bool $isProtectedByDefenceSpell){
+        $this->isProtectedByDefenceSpell = $isProtectedByDefenceSpell;
+    }
+
+    public function getMana(){
+        return $this->mana;
+    }
+
+    public function setMana(int $mana){
+        $this->mana = $mana;
+    }
+
+    public function getManaRecoveryRate(){
+        return $this->manaRecoveryRate;
+    }
+
+    public function setManaRecoveryRate(int $manaRecoveryRate){
+        $this->manaRecoveryRate = $manaRecoveryRate;
+    }
+
+
+    //Methods of Character
+    public function setBonusAttack($enemy): bool
+    {
+        switch ($this->elementarytType) {
+            case 'Water':
+
+                if ($enemy->getElementaryType() === 'Fire') {
+
+                    $this->bonusAttack = true;
+
+                }
+
+                break;
+
+            case 'Fire':
+
+                if ($enemy->getElementaryType() === 'Plant') {
+
+                    $this->bonusAttack = true;
+
+                }
+
+                break;
+
+            case 'Plant':
+
+                if ($enemy->getElementaryType() === 'Water') {
+
+                    $this->bonusAttack = true;
+
+                }
+
+                break;
+
+            default:
+
+                $this->bonusAttack = false;
+
+                break;
+        }
+
+        return $this->bonusAttack;
+    }
+
+    public function attackEnemy(Character $enemy){
+        if($this->setBonusAttack($enemy)){
+            // Text color in green
+            echo "\033[32m";
+            echo $this->name . ' a un bonus d\'attaque de 10 points !' . PHP_EOL;
+            echo "\033[0m";
+            $enemy->receiveAttack($this->physicalAttack, $this->magicalAttack + 10);
+        }else{
+            $enemy->receiveAttack($this->physicalAttack, $this->magicalAttack);
+        }
+        echo $this->name . ' attaque ' . $enemy->getName() . PHP_EOL;
+
+    }
+
+    public function receiveAttack(int $physicalAttack, int $magicalAttack){
+
+        if($this->getIsProtectedByDefenceSpell() == true){
+            $precedentDefence = $this->getDefence();
+
+            //Besoin de limité le plafond/seuil de la défense totale
+            $this->setDefence($this->getDefenceSpell()->getDefenceEfficiency() + $this->getDefence());
+
+            $this->setHealth($this->getHealth() - ($physicalAttack + $magicalAttack) + $this->getDefence());
+
+            $this->setDefence($precedentDefence);
+            $this->setIsProtectedByDefenceSpell(false);
+        }
+        else{
+            $this->setHealth($this->getHealth() - ($physicalAttack + $magicalAttack) + $this->getDefence());
+        }
+
+        echo $this->getName()." possède : ".$this->getHealth()." points de vie".PHP_EOL;
+    }
+
+    //Methods of Spells
+    public function triggerHealingSpell(){
+        $this->setHealth($this->getHealingSpell()->getHealingEfficiency());
+        echo "Le ".$this->getName()." vient de ce soigner de : ".$this->getHealingSpell()->getHealingEfficiency().PHP_EOL;
+
+        //reduction de mana
+        //Passe le tour
+    }
+
+    public function triggerAttackSpell(Character $enemy){
+        $enemy->receiveAttack(0, $this->getAttackSpell()->getAttackEfficiency());
+        echo "Le ".$this->getName()." vient de d'utiliser un sort d'attaque infligeant : ".$this->getAttackSpell()->getAttackEfficiency().PHP_EOL;
+
+        //reduction de mana
+        //Passe le tour
+    }
+
+    public function triggerDefenceSpell(){
+        $this->setIsProtectedByDefenceSpell(true);
+
+        //reduction de mana
+    }
+
+    public function recoverMana(){
+        //Besoin de limité le plafond/seuil de mana
+
+        $this->setMana($this->getMana() + $this->getManaRecoveryRate());
+
+        //reduction de mana
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,119 @@
 <?php
-// Les points d'attaque et de défense que j'ai attribuer aux pokémons sont arbitraires, il faudra les modifier en fonction de l'équilibrage du jeu.
+use App\Characters\Dracofeu;
+use App\Characters\Tortank;
+use App\Characters\Tortipouss;
+use App\Spells\AttackSpell;
+use App\Spells\DefenceSpell;
+use App\Spells\HealingSpell;
 
-echo "test";
+require_once 'autoload.php';
+
+
+// Création des personnages
+$dracofeu = new Dracofeu();
+$tortank = new Tortank();
+
+// Création des sorts
+$fireball = new AttackSpell(
+    attackEfficiency: 20,
+    name: 'Fireball',
+    description: 'Un sort de feu qui inflige des dégâts',
+    manaCost: 10,
+);
+
+$hydrocanon = new AttackSpell(
+    attackEfficiency: 20,
+    name: 'Hydrocanon',
+    description: 'Un sort d\'eau qui inflige des dégâts',
+    manaCost: 10,
+);
+
+$heal = new HealingSpell(
+    healingEfficiency: 20,
+    name: 'Heal',
+    description: 'Un sort de soin qui soigne des dégâts',
+    manaCost: 10,
+);
+
+$shield = new DefenceSpell(
+    defenceEfficiency: 20,
+    name: 'Shield',
+    description: 'Un sort de défense qui réduit les dégâts',
+    manaCost: 10,
+);
+
+$dracofeu->setAttackSpells([
+    $fireball
+]);
+
+$tortank->setAttackSpells([
+    $hydrocanon
+]);
+
+$elementaryTypePlayer = $dracofeu->getElementaryType();
+$elementaryTypeEnemy = $tortank->getElementaryType();
+
+$count = 1;
+
+while ($dracofeu->getHealth() > 0 && $tortank->getHealth() > 0) {
+
+    echo PHP_EOL . 'Tour ' . $count . PHP_EOL;
+
+    $randomSpellTortank = $tortank->getAttackSpells()[array_rand($tortank->getAttackSpells())];
+
+    echo "Choisissez un sort parmi les suivants : " . PHP_EOL;
+    echo "\t1. Fireball" . PHP_EOL;
+    echo "\t2. Heal" . PHP_EOL;
+    echo "\t3. Shield" . PHP_EOL;
+
+    $sortChoisi = readline();
+
+    $previousManaTortank = $tortank->getMana();
+    $tortank->setMana($previousManaTortank - $randomSpellTortank->getManaCost());
+    $tortank->attackEnemy($dracofeu);
+    echo "Tortank a utilisé " . $randomSpellTortank->getName() . " et a infligé " . $randomSpellTortank->getAttackEfficiency() . " points de dégâts à Dracofeu" . PHP_EOL;
+    echo "Dracofeu a perdu " . $randomSpellTortank->getAttackEfficiency() . " points de vie" . PHP_EOL;
+    echo "Dracofeu a maintenant " . $dracofeu->getHealth() . " points de vie" . PHP_EOL;
+    echo "Il reste " . $tortank->getMana() . " points de mana à Tortank" . PHP_EOL . PHP_EOL;
+
+    switch ($sortChoisi) {
+        case 1:
+            $dracofeu->setSpell($fireball);
+
+            $previousMana = $dracofeu->getMana();
+            $dracofeu->setMana($previousMana - $dracofeu->getSpell()->getManaCost());
+
+            echo "Vous avez choisi le sort ";
+            echo "\033[31m";
+            echo $fireball->getName() . PHP_EOL;
+            echo "\033[0m";
+            echo "Il vous reste " . $dracofeu->getMana() . " points de mana" . PHP_EOL;
+
+            $dracofeu->attackEnemy($tortank);
+
+
+            break;
+
+        case 2:
+            echo "Vous avez choisi le sort Heal" . PHP_EOL;
+            break;
+
+        case 3:
+            echo "Vous avez choisi le sort Shield" . PHP_EOL;
+            break;
+
+        default:
+            echo 'Sort non reconnu';
+            exit;
+    }
+    $count++;
+    $dracofeu->setMana($dracofeu->getMana() + $dracofeu->getManaRecoveryRate());
+    $tortank->setMana($tortank->getMana() + $tortank->getManaRecoveryRate());
+}
+
+
+if ($dracofeu->getHealth() > 0) {
+    echo PHP_EOL . "Dracofeu a gagné !" . PHP_EOL;
+} else {
+    echo PHP_EOL . "Tortank a gagné !" . PHP_EOL;
+}
